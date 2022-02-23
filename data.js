1ee32c9a247a59d7d4fd0a904227bf6416864d19
Randy W <rwoster@protonmail.com>
2022-02-22 19:22:06 -0500

    part 2 finished

---
+++ data.js
@@ -0,0 +1,16 @@
+const characterData = {
+    wizard: {
+        name: "Wizard",
+        avatar: "./images/wizard.png",
+        health: 100,
+        diceCount: 3,
+    },
+    orc: {
+        name: "Orc",
+        avatar: "./images/orc.png",
+        health: 100,
+        diceCount: 3,
+    },
+};
+
+export default characterData;
--- index.js
+++ index.js
@@ -1,63 +1,23 @@
+import characterData from "./data.js";
+import Character from "./Character.js";
+
+// create player instance
+let wizard = new Character(characterData.wizard);
+let orc = new Character(characterData.orc);
+
+// render players
+function renderPlayers() {
+    document.getElementById("hero").innerHTML =
+        wizard.getCharacterHtml();
+    document.getElementById("monster").innerHTML =
+        orc.getCharacterHtml();
+}
+renderPlayers();
+
+//
+// event listener
+//
 let attackBtn = document.getElementById("attack-button");
-
-let hero = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "./images/wizard.png",
-    health: 100,
-    diceCount: 3,
-};
-
-let monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "./images/orc.png",
-    health: 100,
-    diceCount: 3,
-};
-
-let turn = true;
 attackBtn.addEventListener("click", () => {
-    if (turn) {
-        turn = false;
-        renderCharacter(hero);
-    } else {
-        turn = true;
-        renderCharacter(monster);
-    }
+    renderPlayers();
 });
-
-function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceCount } = data;
-
-    const diceHtml = getDiceHtml(diceCount);
-
-    document.getElementById(elementId).innerHTML = `
-        <div class="character-card">
-            <h4 class="name">${name}</h4>
-            <img class="avatar" src=${avatar} />
-            <p class="health">health: <b>${health}</b></p>
-            <div class="dice-container">
-                ${diceHtml}
-            </div>
-        </div>
-    `;
-}
-
-renderCharacter(hero);
-renderCharacter(monster);
-
-function getDiceRollArray(diceCount) {
-    const newDiceRolls = new Array(diceCount).fill("0").map(() => {
-        return Math.floor(Math.random() * 6) + 1;
-    });
-    return newDiceRolls;
-}
-
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount)
-        .map((dice) => {
-            return `<div class='dice'>${dice}</div>`;
-        })
-        .join("");
-}
